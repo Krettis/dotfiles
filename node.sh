eb435b3a48a85f46022bfaf0d714db4a7a72ce9a
krettis <krettis@gmail.com>
2015-03-26 21:17:54 +0100

    Delete node totally - function

--- node.sh
+++ node.sh
@@ -4,4 +4,16 @@ function nodeUpdate
   sudo npm install npm -g
 }
 
-//yo, grunt, bower, iojs, express
+function nodeTrash
+{
+  sudo rm /usr/local/bin/npm
+  sudo rm /usr/local/share/man/man1/node.1
+  sudo rm /usr/local/lib/dtrace/node.d
+  sudo rm -rf ~/.npm
+  sudo rm -rf ~/.node-gyp
+  sudo rm /opt/local/bin/node
+  sudo rm /opt/local/include/node
+  sudo rm -rf /opt/local/lib/node_modules
+}
+
+//yo, grunt, bower, iojs, express, n (version manager), nodemon
