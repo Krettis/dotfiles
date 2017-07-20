bee03098168167f2074c4c8ed429727314b61958
Krettis <krettis@gmail.com>
2017-07-20 22:11:25 +0200

    Error message for shell

--- log.sh
+++ log.sh
@@ -22,6 +22,14 @@ function msg_ok {
 	echo -e  "➜\033[1;32m $1 ✔\033[0m";
 }
 
+function msg_error {
+	echo -e  "\⃠\033[1;31m $1 \033[0m";
+}
+
+function msg_warning {
+	echo -e  "⚠\033[1;33m $1 \033[0m";
+}
+
 function msg_prompt {
 	echo -e "➜\033[1;33m $1 \033[0m";
 }
